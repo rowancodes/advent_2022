b6c4c4178e54fd5fde5e35e7dc59d0fa0e425a21
rowancodes <rowan.skellie@gmail.com>
2022-12-11 00:00:06 -0500

    10/part02 finish

--- 10/source.rb
+++ 10/source.rb
@@ -6,14 +6,32 @@ $clock = 0
 $reg_x = 1
 
 $timeline = []
+$crt = []
 
 def parser
     $input = File.read("input.txt")
     $input = $input.split("\n")
 end
 
-def signal_strength
+def draw_crt_pixel
+    sprite_areas = [$reg_x - 1, $reg_x, $reg_x + 1]
+    p sprite_areas
+    col = ($clock-1) % 40
+    p col
+    if sprite_areas.include?(col)
+        $crt.push("#")
+    else
+        $crt.push(".")
+    end
+end
 
+def display_crt
+    p $crt[0..39].join
+    p $crt[40..79].join
+    p $crt[80..119].join
+    p $crt[120..159].join
+    p $crt[160..199].join
+    p $crt[200..239].join
 end
 
 def run_instruction(line)
@@ -23,6 +41,7 @@ def run_instruction(line)
 
     $duration[instruction.to_sym].times do
         $clock += 1
+        draw_crt_pixel
         $timeline.push($reg_x.dup)
     end
 
@@ -44,16 +63,17 @@ def main
         run_instruction(line)
     }
 
-    sum = 0
-    sum += $timeline[20] * 20
-    sum += $timeline[60] * 60
-    sum += $timeline[100] * 100
-    sum += $timeline[140] * 140
-    sum += $timeline[180] * 180
-    sum += $timeline[220] * 220
-
-    p sum
-    p $timeline
+    # part 01
+    # sum = 0
+    # sum += $timeline[20] * 20
+    # sum += $timeline[60] * 60
+    # sum += $timeline[100] * 100
+    # sum += $timeline[140] * 140
+    # sum += $timeline[180] * 180
+    # sum += $timeline[220] * 220
+    # p sum
+    
+    display_crt
 end
 
 main
